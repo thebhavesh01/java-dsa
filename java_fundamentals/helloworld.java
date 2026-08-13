3907e081b7d189b272ace8f76315441797550af1
thebhavesh01 <280848687+thebhavesh01@users.noreply.github.com>
2026-08-13 13:37:09 +0530

    Add Java fundamentals

--- java_fundamentals/helloworld.java
+++ java_fundamentals/helloworld.java
@@ -1,6 +1,7 @@
+package java_fundamentals;
+
 public class helloworld{
     public static void main(String[]args){
     System.out.println("Hello, World!");
  }
 }
-//git push --set-upstream origin maingit log -1 --format=fullergit log -1 --format=fuller
---
+++ java_fundamentals/question3.java
@@ -0,0 +1,10 @@
+package java_fundamentals;
+//add two numbers
+public class question3 {
+    public static void main(String[]args){
+        int a=20;
+        int b=30;
+        int sum=a+b;
+        System.out.println("The sum of two numbers is: "+sum);
+    }
+}
