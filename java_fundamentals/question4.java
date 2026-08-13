db3438d05ca2342e2932fa2ac3acba9c8f179cc6
thebhavesh01 <280848687+thebhavesh01@users.noreply.github.com>
2026-08-13 13:39:30 +0530

    Add java fundamentals

---
+++ java_fundamentals/question4.java
@@ -0,0 +1,11 @@
+package java_fundamentals;
+// multiply two numbers
+public class question4 {
+    public static void main(String[]args){
+        int a=20;
+        int b=30;
+        int product=a*b;
+        System.out.println("The product of two numbers is: "+product);
+    }
+    
+}
